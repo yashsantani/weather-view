7dbaf87034401606a9d6e931d4679f731eed914f
Yash <yashsantani60@gmail.com>
2021-04-09 18:45:28 +0530

    initial commit

--- weather.py
+++ weather.py
@@ -4,16 +4,24 @@ import json
 from PIL import ImageTk,Image
 root=Tk()
 
-#my_img1=ImageTk.PhotoImage(Image.open(r'C:\Users\Yash\Downloads\summerdm.jpg'))
-#my_img3=ImageTk.PhotoImage(Image.open(r'C:\Users\Yash\Downloads\winterdm.jpg'))
-#my_img2=ImageTk.PhotoImage(Image.open(r'C:\Users\Yash\Downloads\autumdm.jpg'))
+#loading images
+my_img1=ImageTk.PhotoImage(Image.open(r'‪img_summer.jpg'))
+my_img3=ImageTk.PhotoImage(Image.open(r'‪img_winter.jpg'))
+my_img2=ImageTk.PhotoImage(Image.open(r'img_autum.jpg'))
 e=Entry(root)
 e.grid(row=0,column=0)
+
+
+#reset function 
+
 def des():
     mylabel.destroy()
     mylabel2.destroy()
     mylabel3.destroy()
     e.delete(0,END)
+
+#fuction to hit api with pincode and get back a json file 
+
 def call():
     url = "http://api.openweathermap.org/data/2.5/weather?zip="+e.get()+",in&appid=e38272bcc6592022ec2c45f73f070397"
     api_request=requests.get(url)
@@ -33,8 +41,15 @@ def call():
         mylabel3=Label(image=my_img3)
         mylabel3.grid(row=2,column=0)
     print(api)
+
+#find button
+
 button1=Button(root,text="find",command=call)
 button1.grid(row=0,column=1)
+
+#reset button
+
 button2=Button(root,text="reset",command=des)
 button2.grid(row=0,column=2)
-mainloop()
+
+root.mainloop()
